cada59ef5a93a662a59d0d70bf0e8a16cd63fb6d
Pasztor Istvan <isticsek@gmail.com>
2022-09-23 19:56:21 +0300

    Slug page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,8 +1,13 @@
+import { GetStaticProps } from 'next';
 import Header from '../../components/Header';
 import { sanityClient, urlFor } from '../../sanity';
 import { Post } from '../../typings';
 
-const PostPage = () => {
+interface Props {
+  post: Post;
+}
+
+const PostPage = ({ post }: Props) => {
   return (
     <main>
       <Header />
@@ -33,5 +38,37 @@ export const getStaticPaths = async () => {
   return {
     paths,
     fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `
+  *[_type == "post" && slug.current == $slug][0] {
+    _id,
+    _createdAt,
+    title,
+    author-> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug,
+    body
+  }
+  `;
+
+  const post = await sanityClient.fetch(query, { slug: params?.slug });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+    },
   };
 };
